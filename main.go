809b6d9a8731c4fd4e61940077912fc446bf16c4
danielb <git@masterunit.net>
2020-10-03 02:11:10 +0200

    regex and line interface

--- main.go
+++ main.go
@@ -7,11 +7,27 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
-	"text/tabwriter"
+	"regexp"
 
 	wf "github.com/danielb42/whiteflag"
 	c "github.com/logrusorgru/aurora/v3"
+)
+
+type (
+	Line        interface{ Format() string }
+	ScalarLine  struct{ Text string }
+	VectorLine  struct{ Text string }
+	HelpComment struct{ Text string }
+	TypeComment struct{ Text string }
+	OtherLine   struct{ Text string }
+)
+
+var (
+	firstLine = true
+	re_scalar = regexp.MustCompile(`^([a-zA-Z0-9_]+) ([0-9\-\.e\+]+)$`)
+	re_vector = regexp.MustCompile(`^([a-zA-Z0-9_]+){(.+)} ([0-9\-\.e\+]+)$`)
+	re_help   = regexp.MustCompile(`^(#) (HELP) (\w+) (.+)$`)
+	re_type   = regexp.MustCompile(`^(#) (TYPE) (\w+) (.+)$`)
 )
 
 func main() {
@@ -38,53 +54,99 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-	defer w.Flush()
+	//TODO
+	//w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, '.', tabwriter.Debug)
+	//defer w.Flush()
+	w := os.Stdout
 
 	for s := bufio.NewScanner(resp.Body); s.Scan(); {
-		line := s.Text()
-
-		if !strings.HasPrefix(line, "#") {
-			fmt.Fprintln(w, colorizeMetric(line))
-			continue
-		}
-
-		if !wf.FlagPresent("nocomments") {
-			if strings.HasPrefix(line, "# HELP") {
-				fmt.Fprint(w, "\n", colorizeComment(line), "\n")
-			} else if strings.HasPrefix(line, "# TYPE") {
-				fmt.Fprintln(w, colorizeComment(line))
-			}
-		}
+		line := parseLine(s.Text())
+		fmt.Fprint(w, line.Format())
 	}
 }
 
-func colorizeComment(text string) string {
-
-	tokens := strings.Split(text, " ")
-
-	if len(tokens) < 4 {
-		return ""
+func parseLine(line string) Line {
+	if re_scalar.MatchString(line) {
+		return ScalarLine{Text: line}
 	}
 
-	return fmt.Sprintf("%s %s %s %s",
-		c.Red("#").String(),
-		c.Red(tokens[1]).String(),
-		c.Yellow(tokens[2]).String(),
-		c.Green(strings.Join(tokens[3:], " ")).String(),
+	if re_vector.MatchString(line) {
+		return VectorLine{Text: line}
+	}
+
+	if re_help.MatchString(line) {
+		return HelpComment{Text: line}
+	}
+
+	if re_type.MatchString(line) {
+		return TypeComment{Text: line}
+	}
+
+	return OtherLine{Text: line}
+}
+
+func (line ScalarLine) Format() string {
+
+	tokens := re_scalar.FindStringSubmatch(line.Text)
+
+	return fmt.Sprintf("%s %s\n",
+		tokens[1],
+		c.Green(tokens[2]).String(),
 	)
 }
 
-func colorizeMetric(text string) string {
+func (line VectorLine) Format() string {
 
-	tokens := strings.Split(text, " ")
+	tokens := re_vector.FindStringSubmatch(line.Text)
 
-	if len(tokens) < 2 {
+	return fmt.Sprintf("%s %s %s\n",
+		tokens[1],
+		c.Magenta(tokens[2]).String(),
+		c.Green(tokens[3]).String(),
+	)
+}
+
+func (line HelpComment) Format() string {
+
+	if wf.FlagPresent("nocomments") {
 		return ""
 	}
 
-	return fmt.Sprintf("%s\t%s",
-		c.Blue(tokens[0]).String(),
-		c.White(tokens[1]).String(),
+	leadingNewline := ""
+
+	if !firstLine {
+		leadingNewline = "\n"
+	} else {
+		firstLine = false
+	}
+
+	tokens := re_help.FindStringSubmatch(line.Text)
+
+	return fmt.Sprintf("%s%s %s %s %s\n",
+		leadingNewline,
+		c.Gray(12, tokens[1]).String(),
+		c.Gray(12, tokens[2]).String(),
+		c.Blue(tokens[3]).String(),
+		c.Green(tokens[4]).String(),
 	)
+}
+
+func (line TypeComment) Format() string {
+
+	if wf.FlagPresent("nocomments") {
+		return ""
+	}
+
+	tokens := re_type.FindStringSubmatch(line.Text)
+
+	return fmt.Sprintf("%s %s %s %s\n",
+		c.Gray(12, tokens[1]).String(),
+		c.Gray(12, tokens[2]).String(),
+		c.Blue(tokens[3]).String(),
+		c.Green(tokens[4]).String(),
+	)
+}
+
+func (line OtherLine) Format() string {
+	return c.Red(line.Text).String() + "\n"
 }
